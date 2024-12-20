be861baef21e0638923e0120767000cc4f8ae8e8
William Brown <wsbrown.colo@gmail.com>
2024-12-20 16:05:51 -0700

    Create Boot MVC project, link JPA project dependency

---
+++ ColoradoSports/src/main/java/com/skilldistillery/sports/ColoradoSportsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoradoSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoradoSportsApplication.class, args);
+	}
+
+}
---
+++ ColoradoSports/src/test/java/com/skilldistillery/sports/ColoradoSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoradoSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
