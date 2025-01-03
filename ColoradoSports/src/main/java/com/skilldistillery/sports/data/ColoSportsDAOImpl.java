426c15d7a381c15d37881238b1c26b98b7610d6f
William Brown <wsbrown.colo@gmail.com>
2025-01-03 12:05:39 -0600

    completed DAOImpl, finished update and delete methods

--- ColoradoSports/src/main/java/com/skilldistillery/sports/data/ColoSportsDAOImpl.java
+++ ColoradoSports/src/main/java/com/skilldistillery/sports/data/ColoSportsDAOImpl.java
@@ -12,17 +12,16 @@ import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
-public class ColoSportsDAOImpl implements ColoSportsDAO{
-	
+public class ColoSportsDAOImpl implements ColoSportsDAO {
+
 	@PersistenceContext
 	private EntityManager em;
 
 	@Override
 	public List<Team> findAll() {
-		
+
 		String allTeams = "SELECT t FROM Team t";
-		List<Team> results = em.createQuery(allTeams, Team.class)
-							   .getResultList();
+		List<Team> results = em.createQuery(allTeams, Team.class).getResultList();
 		return results;
 	}
 
@@ -30,26 +29,45 @@ public class ColoSportsDAOImpl implements ColoSportsDAO{
 	public Team findById(int teamId) {
 		return em.find(Team.class, teamId);
 	}
-	
-	//NO transaction.begin/commit
-	//NO em.close
+
+	// NO transaction.begin/commit
+	// NO em.close
 
 	@Override
 	public Team create(Team newTeam) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(newTeam);
+		return newTeam;
 	}
 
 	@Override
-	public Team update(int teamId, Team updatingTeam) {
-		// TODO Auto-generated method stub
-		return null;
+	public Team update(int teamId, Team updatedTeam) {
+		Team managedTeam = em.find(Team.class, teamId);
+		managedTeam.setName(updatedTeam.getName());
+		managedTeam.setLeague(updatedTeam.getLeague());
+		managedTeam.setSportName(updatedTeam.getSportName());
+		managedTeam.setOwner(updatedTeam.getOwner());
+		managedTeam.setManager(updatedTeam.getManager());
+		managedTeam.setCoach(updatedTeam.getCoach());
+		managedTeam.setNumberChampionships(updatedTeam.getNumberChampionships());
+		managedTeam.setVenue(updatedTeam.getVenue());
+		managedTeam.setCity(updatedTeam.getCity());
+		managedTeam.setNumberHOF(updatedTeam.getNumberHOF());
+		managedTeam.setEstablished(updatedTeam.getEstablished());
+		managedTeam.setActive(updatedTeam.getActive());
+		managedTeam.setValue(updatedTeam.getValue());
+		em.persist(managedTeam);
+		return managedTeam;
 	}
 
 	@Override
 	public boolean deleteById(int teamId) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			Team managedTeam = em.find(Team.class, teamId);
+			em.remove(managedTeam);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
 	}
 
 }
